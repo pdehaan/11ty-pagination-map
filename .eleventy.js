4c324497135ec11260de9c461ff274421d630685
Peter deHaan <peter@deseloper.com>
2020-01-27 01:56:42 -0800

    Convert some Nunjucks templates to HTML because Prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = eleventyConfig => {
+  // https://www.11ty.dev/docs/data-deep-merge/
+  // This lets us split the front-matter between templates and config JSON.
+  eleventyConfig.setDataDeepMerge(true);
+
   return {
+    htmlTemplateEngine: "njk",
     dir: {
       data: "../_data",
       input: "src"
